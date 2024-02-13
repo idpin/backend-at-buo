3a50073f0cdeadcd6784672985b409ce88ec4324
idpin <pinoignacio@uniovi.es>
2024-02-13 11:58:08 +0100

    app simple service get all and get issn

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const app = express();
-const cors = require("cors");
+let cors = require('cors')
 app.use(express.json())
+app.use(cors())
 
 const port = 4000;
 
--- routers/routerJournals.js
+++ routers/routerJournals.js
@@ -3,17 +3,13 @@ const database = require("../database")
 
 const routerJournals = express.Router();
 
-
 routerJournals.get("/", async (req,res) => {
-    console.log(req.query);
     database.connect();
-
-    let journals = await database.query("SELECT * FROM journals WHERE issn like ? or title like ?", ['%'+req.query.search+'%','%'+req.query.search+'%'])
+    let journals = await database.query("SELECT * FROM journals")
 
     database.disconnect();
     res.json(journals)
 })
-
 
 routerJournals.get("/:issn", async (req, res) => {
     let issn = req.params.issn;
@@ -35,24 +31,5 @@ routerJournals.get("/:issn", async (req, res) => {
     res.json(journals)
 })
 
-routerJournals.get("/:title", async (req, res) => {
-    let title = req.params.title;
-    if ( title == undefined){
-        return res.status(400).json({ error : "no title param"})
-    }
-    database.connect();
-
-    let journals = await database.query("SELECT * FROM journals WHERE title = ?", [title])
-    if ( journals.length < 1){
-        database.disconnect();
-        return res.status(400).json({ error : "no journals with this title"})
-    } else {
-        database.disconnect();
-        return res.json(journals[0])
-    }
-    
-    database.disconnect();
-    res.json(journals)
-})
 
 module.exports = routerJournals;
